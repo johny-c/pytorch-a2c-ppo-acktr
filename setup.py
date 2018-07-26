fce9e1b5bd08150b051f86e90d7a4eb748c3d5c1
John Chiotellis <chiotell@in.tum.de>
2018-07-26 13:27:05 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='pytorch-a2c-ppo-acktr',
+      version='0.0.1',
+      dependency_links=['git+https://github.com/johny-c/pytorch-a2c-ppo-acktr.git@master'],
+      install_requires=['gym']
+)

37e288cb91b92af336451decf76040354f773994
John Chiotellis <chiotell@in.tum.de>
2018-07-26 15:54:29 +0200

    comment out dependency link

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ from setuptools import setup
 
 setup(name='pytorch-a2c-ppo-acktr',
       version='0.0.1',
-      dependency_links=['git+https://github.com/johny-c/pytorch-a2c-ppo-acktr.git@master'],
+      # dependency_links=['git+https://github.com/johny-c/pytorch-a2c-ppo-acktr.git@master'],
       install_requires=['gym']
 )
